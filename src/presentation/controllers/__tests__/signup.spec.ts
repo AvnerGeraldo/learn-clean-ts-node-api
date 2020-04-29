2c44c6033fc484c655c6a2787c89721924df9c02
Avner <avner.conceicao@hotmail.com>
2020-04-29 18:16:47 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/__tests__/signup.spec.ts
+++ src/presentation/controllers/__tests__/signup.spec.ts
@@ -30,4 +30,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  // Test if fields are sent by the route
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController() // sut = System under test
+    const httpRequest = { // Post Request
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
