6e9ac0d5d4ae51c1336ef02ee519cc569b7cc602
Avner <avner.conceicao@hotmail.com>
2020-06-12 15:02:28 -0300

    refactor: avoid main root for test coverage and eliminate warnings from jest

--- jest.config.js
+++ jest.config.js
@@ -3,21 +3,24 @@
 
 module.exports = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest'
   },
 
-  preset: "@shelf/jest-mongodb",
-};
+  preset: '@shelf/jest-mongodb'
+}
